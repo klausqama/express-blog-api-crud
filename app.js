08b85bec9bad9721716ed71eb539e11ac887de27
klausqama <klaussqama@gmail.com>
2024-11-21 18:56:17 +0100

    add body parser

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const postsRouter = require('./routers/posts.js');
 
+app.use(express.json());
+
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
